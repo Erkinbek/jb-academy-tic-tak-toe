f527312a8f754ac7f14206b9e17e0517d23a4b73
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-07-14 17:49:16 +0800

    Stage #2

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,16 +1,22 @@
 package tictactoe;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String[] lines = {str.substring(0,3), str.substring(3,6), str.substring(6,9)};
+        System.out.print("---------\n");
         for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(
-                    ThreadLocalRandom.current().nextInt(0,2) > 0 ? "X" : "O"
-                );
+                System.out.print(lines[i].charAt(j));
+                if (j != 2) System.out.print(" ");
             }
+            System.out.print(" |");
             System.out.print("\n");
         }
+        System.out.print("---------");
     }
 }

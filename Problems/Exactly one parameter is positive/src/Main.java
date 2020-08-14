318f0896c84444828b06e9f6b667fe0f780565c6
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-08-14 12:11:43 +0800

    Relational operators => Exactly one parameter is positive   (POTD)

---
+++ Problems/Exactly one parameter is positive/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int res = 0;
+        while (scanner.hasNextInt()) {
+            if (scanner.nextInt() > 0) {
+                res++;
+            }
+        }
+        System.out.println(res < 2);
+    }
+}
